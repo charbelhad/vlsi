aa1a46a3dfaa43180219316af75f810b1a2c398d
ce2424 <ce2424@ecs02.poly.edu>
2026-05-16 23:05:35 -0400

    Ignore generated setup and utility folders

--- mbist/rm_dc_scripts/mpc.tcl
+++
@@ -1,9 +0,0 @@
-##########################################################################################
-# Minimal physical constraints for Design Compiler methodology scripts
-# Script: mpc.tcl
-# Version: U-2022.12
-# Copyright (C) 2007-2023 Synopsys, Inc. All rights reserved.
-##########################################################################################
-
-
-
